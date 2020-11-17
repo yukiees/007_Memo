19555272ac0b534cb46f6ff208d1d5a34b8484ca
yukiees <yukiii8989@gmail.com>
2020-11-17 12:41:19 +0900

    Memo comp

--- 007_Memo/MemoViewController.swift
+++ 007_Memo/MemoViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class MemoViewController: UIViewController {
+class MemoViewController: UIViewController,UITextFieldDelegate {
 
     @IBOutlet weak var titleTextField: UITextField!
     @IBOutlet weak var contentTextView: UITextView!
@@ -17,14 +17,35 @@ class MemoViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        titleTextField.text = (saveData.object(forKey: "title") as! String)
-        contentTextView.text = (saveData.object(forKey: "content") as! String) 
+        titleTextField.text = (saveData.object(forKey: "title") as? String)
+        contentTextView.text = (saveData.object(forKey: "content") as? String)
+        titleTextField.delegate = self
     }
     
     @IBAction func saveMemo(_ sender: Any) {
         //Userdefaultsに書き込み
         saveData.set(titleTextField.text, forKey: "title")
         saveData.set(contentTextView.text, forKey: "content")
+        
+        let alert: UIAlertController = UIAlertController(title:"保存",message: "メモの保存が完了しました",preferredStyle: .alert)
+        
+        alert.addAction(
+                UIAlertAction(
+                            title:"OK",
+                            style: .default,
+                            handler: { action in
+                                //ボタンが押されたときの動作
+                                self.navigationController?.popViewController(animated: true)
+                            }
+                )
+        )
+        
+        present(alert, animated: true, completion:nil)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
     }
     
 
